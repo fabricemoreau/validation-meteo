0aee95293cd4402e77bd33a4394b6d9d5b279b11
fabricemoreau <f.moreau@arvalis.fr>
2025-02-17 09:09:31 +0100

    extract subdata by years

--- src/features/extract_subdata.py
+++ src/features/extract_subdata.py
@@ -4,43 +4,41 @@ Script pour extraire un sous-ensemble du jeu de données de 5Go
 import pandas as pd
 import duckdb
 
+# sélection d'un échantillon de stations (1/5 du jeu de données)
+liste_stationspresentes = duckdb.sql("SELECT DISTINCT codearvalis FROM './data/raw/donneesmeteo_2010-2024_completes.csv'").to_df().codearvalis.astype('int').tolist()
+print("nombre de stations présentes", len(liste_stationspresentes))
+stations_presentes = pd.read_csv("./data/raw/stationsmeteo.csv", sep=';')
+stations_presentes = stations_presentes[stations_presentes.Station.isin(liste_stationspresentes)]
+stations_presentes.to_csv('./data/raw/stationsmeteo_2010-2024.csv', sep=';')
+# on prend 1/5 des stations =363 pour avoir un jeu de données d'environ 600Mo
+stations_echantillon360 = stations_presentes.sample(363,  random_state = 42)
+stations_echantillon360.to_csv('./data/raw/stationsmeteo_363.csv', sep=';')
+
+query = """
+    SELECT * 
+    FROM './data/raw/donneesmeteo_2010-2024_completes.csv'
+    WHERE libellecourt IN ['TN', 'TX', 'TM', 'RR', 'GLOT', 'ETP'] AND
+          codearvalis IN ("""
+query += ','.join(map(str, stations_echantillon360.Station.unique().tolist())) + ")"
+donnees_363 = duckdb.sql(query)
+donnees_363.to_csv('./data/raw/donneesmeteo_2010-2024_363stations.csv', sep=';', timestamp_format='%Y-%m-%d')
+
+# sélection par groupe d'années: 3 ans
+duree = 3
 duckdb.read_csv("./data/raw/donneesmeteo_2010-2024_completes.csv", sep=';')
-query = """
+for annee_deb in range(2010, 2025, duree):
+    query = """
     SELECT * 
-    FROM './data/raw/donneesmeteo_2010-2024_completes.csv'
-    WHERE EXTRACT('YEAR' FROM datemesure) IN (2011, 2012, 2013, 2014, 2015) 
-       AND libellecourt IN ['TN', 'TX', 'TM', 'RR', 'GLOT', 'ETP']
+FROM './data/raw/donneesmeteo_2010-2024_completes.csv'
+WHERE EXTRACT('YEAR' FROM datemesure) IN ("""
+    query += ','.join(map(str, list(range(annee_deb, annee_deb + duree - 1, 1))))
+    query +=""") 
+    AND libellecourt IN ['TN', 'TX', 'TM', 'RR', 'GLOT', 'ETP']
 """
-df = duckdb.sql(query)
-df.to_csv('./data/raw/donneesmeteo_2011-2015_completes_v2.csv', sep=';', timestamp_format='%Y-%m-%d')
-
-stations = pd.read_csv("./data/raw/stationsmeteo.csv", sep=';')
-stations_selected = stations.Station.sample(500,  random_state = 42)
-
-query = """
-    SELECT * 
-    FROM './data/raw/donneesmeteo_2010-2024_completes.csv'
-    WHERE codearvalis IN ("""
-query += ','.join(map(str, stations_selected.tolist())) + ")"
-df = duckdb.sql(query)
-df.to_csv('./data/raw/donneesmeteo_2010-2024_500stations.csv', sep=';', timestamp_format='%Y-%m-%d')
-
-# pip install basemap
-import pandas as pd
-from mpl_toolkits.basemap import Basemap
-import matplotlib.pyplot as plt
-sm = pd.read_csv('./data/raw/stationsmeteo.csv', sep = ';')
-# On se restreint aux stations météo ouvertes actuellement
-sm_ouverte = sm[sm['En Service'] == 'Ouverte']
-sm_ouverte = stations[stations.Station.isin(stations_selected)]
-plt.figure(figsize=(10, 10))
-m =  Basemap(llcrnrlon=-5.,llcrnrlat=42.,urcrnrlon=9.5,urcrnrlat=51.,
-             resolution='i', projection='tmerc', lat_0 = 39.5, lon_0 = -3.25)
-#m.etopo()
-m.drawmapboundary(fill_color='aqua')
-m.fillcontinents(color='coral',lake_color='aqua')
-m.drawcoastlines()
-m.drawcountries()
-x, y = m(sm_ouverte.Longitude, sm_ouverte.Latitude)
-m.scatter(x, y, marker='D',color='m')
-plt.show()
+    df = duckdb.query(query)
+    df.to_csv('./data/raw/donneesmeteo_' + str(annee_deb) + '-' + str(annee_deb + duree - 1) + '_completes.csv', sep=';', timestamp_format='%Y-%m-%d')
+    liste_stationspresentes = df.to_df().codearvalis.unique().astype('int').tolist()
+    
+    stations_presentes = pd.read_csv("./data/raw/stationsmeteo.csv", sep=';')
+    stations_presentes = stations_presentes[stations_presentes.Station.isin(liste_stationspresentes)]
+    stations_presentes.to_csv('./data/raw/stationsmeteo_' + str(annee_deb) + '-' + str(annee_deb + duree - 1) + '.csv', sep=';')
