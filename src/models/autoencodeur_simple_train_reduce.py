34f5d9e5276b9063c2cb14a93d6a65b6d60366ef
fabricemoreau <f.moreau@arvalis.fr>
2025-03-14 08:29:00 +0100

    autoencodeur: code clean-up

---
+++ src/models/autoencodeur_simple_train_reduce.py
@@ -0,0 +1,115 @@
+""" autoencodeur pour un plusieurs paramètres"""
+import numpy as np
+import pandas as pd
+
+# pour mygaussiannoise
+from keras.src import backend
+from keras.src import ops
+from keras.src import layers
+from keras.saving import register_keras_serializable
+
+from tensorflow.keras.models import Model
+from tensorflow.keras.layers import Input
+from tensorflow.keras.layers import Dense
+from tensorflow.keras.layers import GaussianNoise
+from tensorflow.keras.layers import Dropout, BatchNormalization
+from tensorflow.keras.layers import LeakyReLU
+from keras.optimizers import Adam
+from tensorflow.keras.callbacks import EarlyStopping, ReduceLROnPlateau, ModelCheckpoint
+
+
+method  = 'autoencsimple'
+path    = './data/processed/' + method
+
+parametres = ['ETP', 'GLOT', 'TN', 'TX'] #['ETP', 'GLOT', 'TN', 'TX'] 
+
+fichier_suffixe = '-'.join(parametres) + '_' + str(2010) + '-' + str(2022)
+
+
+# les colonnes doivent être dans l'ordre: parametres + meta_features
+X_train = np.load(path + '/np_xtrain_' + fichier_suffixe + '.npy')
+X_test = np.load(path + '/np_xtest_' + fichier_suffixe + '.npy')
+
+nfeatures = X_train.shape[1]
+noutputs = len(parametres)
+
+# on ne veut prédire que les paramètres, pas les meta_features
+y_train = X_train[:,0:noutputs]
+y_test = X_test[:,0:noutputs]
+
+
+"""
+inputs = Input(shape=(nfeatures, ), name = 'input')
+e = MyGaussianNoise([0.03, 0.02, 0.006, 0.003, 0, 0, 0, 0, 0, 0])(inputs) # 0.03
+e = Dense(nfeatures *2, name = 'encoder_l1')(e)
+#e = Dropout(rate = 0.2, name = 'drop1')(e)
+e = BatchNormalization(name = 'batchnorm1')(e)
+e = LeakyReLU(name = 'leakyrelu1')(e)
+e = Dense(nfeatures, name = 'encoder2')(e)
+#e = Dropout(rate = 0.2)(e)
+e = BatchNormalization(name = 'batchnorm2')(e)
+e = LeakyReLU(name = 'leakyrelu2')(e)
+bottleneck = Dense(nfeatures, name = 'output')(e)
+#d = Dropout(rate = 0.2)(bottleneck)
+d = Dense(noutputs)(bottleneck)
+d = BatchNormalization()(d)
+d = LeakyReLU()(d)
+# decoder level 2
+d = Dense(noutputs*2)(d)
+d = BatchNormalization()(d)
+d = LeakyReLU()(d)
+outputs = Dense(noutputs, activation='linear')(d)
+model = Model(inputs=inputs, outputs=outputs) 
+model.compile(optimizer=Adam(learning_rate=1E-3), loss='mse', metrics=['mae'])
+#model.compile(optimizer=Adam(learning_rate=1E-2), loss='mse', metrics=['mae'])
+"""
+
+# define model
+inputs = Input(shape=(nfeatures, ), name = 'input')
+e = MyGaussianNoise([0.03, 0.02, 0.007, 0.003, 0, 0, 0, 0, 0, 0])(inputs) # 0.03
+e = Dense(nfeatures // 2, name = 'encoder_l1')(e)
+#e = Dropout(rate = 0.2, name = 'drop1')(e)
+e = BatchNormalization(name = 'batchnorm1')(e)
+e = LeakyReLU(name = 'leakyrelu1')(e)
+e = Dense(nfeatures // 3, name = 'encoder2')(e)
+#e = Dropout(rate = 0.2)(e)
+e = BatchNormalization(name = 'batchnorm2')(e)
+e = LeakyReLU(name = 'leakyrelu2')(e)
+#e = Dropout(rate = 0.2)(e)
+bottleneck = Dense(nfeatures // 4, name = 'output')(e)
+#d = Dropout(rate = 0.2)(bottleneck)
+d = Dense(noutputs  // 3)(bottleneck)
+d = BatchNormalization()(d)
+d = LeakyReLU()(d)
+# decoder level 2
+d = Dense(noutputs // 2)(d)
+d = BatchNormalization()(d)
+d = LeakyReLU()(d)
+outputs = Dense(noutputs, activation='linear')(d)
+model = Model(inputs=inputs, outputs=outputs) 
+model.compile(optimizer=Adam(learning_rate=1E-3), loss='mse', metrics=['mae'])
+
+model.summary()
+
+early_stopping = EarlyStopping(monitor = 'val_loss',
+                           min_delta = 1E-7,
+                           patience = 3,
+                           mode = 'min',
+                           verbose = 1)
+reduce_learning_rate = ReduceLROnPlateau(monitor = 'val_loss',
+                               min_delta = 1E-5, # essayer 1E-6 au lieu de 1E-5
+                               patience = 2,
+                               factor = 0.1, 
+                               min_lr = 1E-6,# ajouté
+                               #cooldown = 2,						
+                               verbose = 1)
+checkpoint = ModelCheckpoint(path + '/autoencodeur_reduce_checkpoint_' + fichier_suffixe + '.keras', 
+                                save_best_only=True, 
+                                monitor='val_loss',
+                                mode='min',
+                                verbose = 1)
+history = model.fit(X_train, y_train, 
+                    validation_data = (X_test, y_test),
+                    callbacks = [early_stopping, reduce_learning_rate, checkpoint],
+                    batch_size=128, epochs=20)
+pd.DataFrame(history.history).to_csv(path + '/autoencodeur_reducte_history_' + fichier_suffixe + '.csv', sep = ';', index = False)
