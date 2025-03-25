b85a1e5edcdc0def6c72c2bf6314e2d47d571b1f
fabricemoreau <f.moreau@arvalis.fr>
2025-03-25 13:36:04 +0100

    autoencoder: train val test sets corrected

--- src/models/autoencodeur_simple_eval.py
+++ src/models/autoencodeur_simple_eval.py
@@ -17,27 +17,27 @@ from sklearn.metrics import classification_report, accuracy_score, precision_sco
 from sklearn.metrics import roc_auc_score, matthews_corrcoef
 from sklearn.metrics import precision_recall_fscore_support
 
-method  = 'autoencsimple'
+method  = 'autoencoder'
 path    = './data/processed/' + method
 
 parametres = ['ETP', 'GLOT', 'TN', 'TX'] 
 
-fichier_suffixe = '-'.join(parametres) + '_' + str(2010) + '-' + str(2022)
+fichier_suffixe = '-'.join(parametres) + '_' + str(2010) + '-' + str(2022) + '-' + str(0.1)
 
 # les colonnes doivent être dans l'ordre: parametres + meta_features
 X_train = np.load(path + '/np_xtrain_' + fichier_suffixe + '.npy')
-X_test = np.load(path + '/np_xtest_' + fichier_suffixe + '.npy')
 X_val = np.load(path + '/np_xval_' + fichier_suffixe + '.npy')
-X_val_recentes = np.load(path + '/np_xval_recentes_' + fichier_suffixe + '.npy')
+X_test = np.load(path + '/np_xtest_' + fichier_suffixe + '.npy')
+X_test_recentes = np.load(path + '/np_xtest_recentes_' + fichier_suffixe + '.npy')
 
 nfeatures = X_train.shape[1]
 noutputs = len(parametres)
 
 # on ne veut prédire que les paramètres, pas les meta_features
 y_train = X_train[:,0:noutputs]
-y_test = X_test[:,0:noutputs]
 y_val = X_val[:,0:noutputs]
-y_val_recentes = X_val_recentes[:,0:noutputs]
+y_test = X_test[:,0:noutputs]
+y_test_recentes = X_test_recentes[:,0:noutputs]
 
 
 scaler_param = joblib.load(path + '/joblib_scaler_param_' + fichier_suffixe + '.gz')
@@ -63,15 +63,20 @@ print("seuil d'anomalie", threshold)
 # on accepte 10 d'erreur
 #threshold = np.quantile(train_ae_loss, 0.98, axis = 0)
 
-plt.figure()
-plt.hist(train_ae_loss, bins=50, label = parametres)
-#plt.plot(threshold[0], [300], label = "threshold 0")
-#plt.plot(threshold[1], [300], label = "threshold 1")
-plt.xlabel("Train Absolute Error loss")
-plt.ylabel("No of samples")
-plt.legend()
+plt.figure(figsize=(15, 15))
+for i, param in enumerate(parametres):
+    ax = plt.subplot(2, 2, i + 1)
+    ax.hist(train_ae_loss, bins=50, label = parametres)
+    ax.axvline(threshold[i], ymax = ax.get_ylim()[1] * 0.9, linestyle = ':', color = 'red')
+    ax.text(threshold[i], ax.get_ylim()[1] * 0.9, 'Seuil anomalie max', size = 11, color = 'red')
+    ax.axvline(np.quantile(train_ae_loss, 0.9, axis = 0)[i], ymax = ax.get_ylim()[1] * 0.7, linestyle = ':', color = 'purple')
+    ax.text(np.quantile(train_ae_loss, 0.98, axis = 0)[i], ax.get_ylim()[1] * 0.7, 'Seuil anomalie décile 9', size = 11, color = 'purple')
+    
+    ax.set_xlabel("Train Absolute Error loss")
+    ax.set_ylabel("No of samples")
+    ax.set_title("seuils définis par rapport aux résidus du jeu d'entrainement")
 plt.savefig(path + '/train_ae_loss_' + fichier_suffixe + '.png')
-plt.show()
+#plt.show()
 
 plt.figure(figsize=(15,15))
 for i, param in enumerate(parametres):
@@ -79,8 +84,9 @@ for i, param in enumerate(parametres):
     ax.plot(y_train[:, i], X_train_pred[:, i], '.')
     ax.title.set_text(param)
 plt.savefig(path + '/residus_' + fichier_suffixe + '.png')
-plt.show()
+#plt.show()
 
+"""
 ### alternative: calculer erreur cumulée de chaque séquence
 # https://www.tensorflow.org/tutorials/generative/autoencoder?hl=fr
 # https://anomagram.fastforwardlabs.com/#/
@@ -101,7 +107,7 @@ plt.ylabel("No of examples")
 plt.legend()
 plt.savefig(path + '/train_total_loss_' + fichier_suffixe + '.png')
 plt.show()
-
+"""
 
 
 
@@ -129,164 +135,108 @@ graph_sequence(y_train[0], 'sequence_1_' + fichier_suffixe + '.png', X_train_pre
 graph_sequence(y_train[38440], 'sequence_38441_' + fichier_suffixe + '.png', X_train_pred[38440])
 graph_sequence(scaler_param.inverse_transform(y_train[0].reshape(1, -1)).reshape(-1), 'sequence_1_denormalise_' + fichier_suffixe  +'.png', scaler_param.inverse_transform(X_train_pred[0].reshape(1, -1)).reshape(-1))
 
-X_test_pred = model.predict(X_test)
-test_ae_loss_param = np.abs(X_test_pred - y_test)
+X_val_pred = model.predict(X_val)
+val_ae_loss_param = np.abs(X_val_pred - y_test)
 
 
-plt.figure()
-plt.hist(test_ae_loss_param, bins=50)
-plt.xlabel("test Absolute Error loss")
-plt.ylabel("No of samples")
-plt.savefig(path + '/test_ae_loss_' + fichier_suffixe + '.png')
-plt.show()
+plt.figure(figsize=(15,15))
+for i, param in enumerate(parametres):
+    ax = plt.subplot(2, 2, i + 1)
+    ax.hist(val_ae_loss_param, bins=50, label = parametres)
+    ax.axvline(threshold[i], ymax = ax.get_ylim()[1] * 0.9, linestyle = ':', color = 'red')
+    ax.text(threshold[i], ax.get_ylim()[1] * 0.9, 'Seuil anomalie max', size = 11, color = 'red')
+    ax.axvline(np.quantile(train_ae_loss, 0.9, axis = 0)[i], ymax = ax.get_ylim()[1] * 0.7, linestyle = ':', color = 'purple')
+    ax.text(np.quantile(train_ae_loss, 0.98, axis = 0)[i], ax.get_ylim()[1] * 0.7, 'Seuil anomalie décile 9', size = 11, color = 'purple')
+    ax.set_xlabel("Train Absolute Error loss")
+    ax.set_ylabel("No of samples")
+    ax.set_title("seuils définis par rapport aux résidus du jeu de validation")
+
+plt.savefig(path + '/val_ae_loss_' + fichier_suffixe + '.png')
+#plt.show()
 
 
 # Detect all the samples which are anomalies.
-anomalies = test_ae_loss_param > threshold
+anomalies = val_ae_loss_param > threshold
 print("Number of anomaly samples: ", np.sum(anomalies))
 print("Indices of anomaly samples: ", np.where(anomalies))
 for i, param in enumerate(parametres):
     print(param)
-    print(pd.crosstab([False] * X_test.shape[0], anomalies[:, i].tolist()))
+    print(pd.crosstab([False] * X_val.shape[0], anomalies[:, i].tolist()))
     
+"""
 ## anomalies from total threshold
-test_loss = tf.keras.losses.mae(y_test, X_test_pred)
+val_loss = tf.keras.losses.mae(y_test, X_val_pred)
 plt.figure()
-plt.hist(test_loss, bins=50)
+plt.hist(val_loss, bins=50)
 plt.axvline(threshold_total, ymax = 10000, linestyle = ':', color = 'red')
 plt.text(threshold_total, 10000, 'Seuil anomalie', size = 11, color = 'red')
-plt.xlabel("Test loss")
+plt.xlabel("Val loss")
 plt.ylabel("No of samples")
-plt.savefig(path + '/test_total_loss_' + fichier_suffixe + '.png')
+plt.savefig(path + '/val_total_loss_' + fichier_suffixe + '.png')
 plt.show()
 
-
-
 # test seuils
-for quant in [0.9]: #[0.9, 0.95, 0.96, 0.97, 0.98, 0.99]:
+for quant in [0.9, 0.95, 0.96, 0.97, 0.98, 0.99]:
     print("quantile", quant)
-    threshold_total = np.quantile(train_loss, quant)
+    threshold_total = np.quantile(val_loss, quant)
     anomalies_total = train_loss > threshold_total
     print("Number of anomaly samples: ", np.sum(anomalies_total))
     print("Indices of anomaly samples: ", np.where(anomalies_total))
-    print(pd.crosstab([False] * X_test.shape[0], anomalies_total.numpy().tolist()))
-    #print(pd.crosstab([False] * X_test.shape[0], anomalies_total.numpy().tolist()))
-    
+    print(pd.crosstab([False] * X_val.shape[0], anomalies_total.numpy().tolist()))
+    #print(pd.crosstab([False] * X_val.shape[0], anomalies_total.numpy().tolist()))
+"""    
 
 
 
 ##### validation
-X_val_pred = model.predict(X_val)
-val_ae_loss_param = np.abs(X_val_pred - y_val)
-for i in [0.9]: #np.arange(1, 0.895, -0.005): # [1]:
+X_test_pred = model.predict(X_test)
+test_ae_loss_param = np.abs(X_test_pred - y_val)
+for i in np.arange(1, 0.895, -0.005): # [1]:
     threshold = np.quantile(train_ae_loss, i, axis = 0)
-    anomalie_val = pd.DataFrame(np.where(val_ae_loss_param > threshold, 1, 0), columns = [param + '_anomaly_pred' for param in parametres])
-    anomalie_val['anomaly_pred'] = np.where(anomalie_val[[param + '_anomaly_pred' for param in parametres]].sum(axis = 1) > 0, 1, 0)
-    X_val_pred_unscaled = pd.DataFrame(scaler_param.inverse_transform(X_val_pred), columns = [param + '_pred' for param in parametres])
-    val = pd.read_csv(path + '/val_preprocessed_' + fichier_suffixe + '.csv', sep = ';')
-    val = pd.concat([val, anomalie_val, X_val_pred_unscaled], axis = 1)
-    val['anomaly'] = np.where(val[[param + '_anomaly' for param in parametres]].sum(axis = 1) > 0, 1, 0)
+    anomalie_test = pd.DataFrame(np.where(test_ae_loss_param > threshold, 1, 0), columns = [param + '_anomaly_pred' for param in parametres])
+    anomalie_test['anomaly_pred'] = np.where(anomalie_test[[param + '_anomaly_pred' for param in parametres]].sum(axis = 1) > 0, 1, 0)
+    X_test_pred_unscaled = pd.DataFrame(scaler_param.inverse_transform(X_test_pred), columns = [param + '_pred' for param in parametres])
+    test = pd.read_csv(path + '/test_preprocessed_' + fichier_suffixe + '.csv', sep = ';')
+    test = pd.concat([test, anomalie_test, X_test_pred_unscaled], axis = 1)
+    test['anomaly'] = np.where(test[[param + '_anomaly' for param in parametres]].sum(axis = 1) > 0, 1, 0)
     print("quantile seuil:", i)
-    print(pd.crosstab(val['anomaly'], val['anomaly_pred']))
-    print(classification_report(val['anomaly'], val['anomaly_pred']))
-    print('precision', precision_score(val['anomaly'], val['anomaly_pred'], zero_division=0))
-    print('recall', recall_score(val['anomaly'], val['anomaly_pred'], zero_division=0))
-    print(f1_score(val['anomaly'], val['anomaly_pred']))
-    #val.to_csv(path + '/val_result_' + fichier_suffixe + '_' + str(i) + '.csv', sep = ';', index = False)
+    print(pd.crosstab(test['anomaly'], test['anomaly_pred']))
+    print(classification_report(test['anomaly'], test['anomaly_pred']))
+    print('precision', precision_score(test['anomaly'], test['anomaly_pred'], zero_division=0))
+    print('recall', recall_score(test['anomaly'], test['anomaly_pred'], zero_division=0))
+    print(f1_score(test['anomaly'], test['anomaly_pred']))
+    #test.to_csv(path + '/test_result_' + fichier_suffixe + '_' + str(i) + '.csv', sep = ';', index = False)
 
-# on retire toutes les anomalies plus petites que l'écart type du paramètre
-train_df = pd.read_csv(path + '/train_preprocessed_' + fichier_suffixe + '.csv', sep = ';')
-std_threshold = {
-    param: train_df[param + '_origine'].std() * 0.1 for param in parametres
-}
-#train.to_csv(path + '/train_preprocessed_' + fichier_suffixe + '.csv', sep = ';', index = False)
 
-for param in parametres:
-    print(param)
-    #print(FN[param + '_anomaly_pred'].value_counts(normalize=True))
-    # on retire des anomalies les valeur corrigées trop proches des valeurs d'origine
-    val.loc[np.abs(val[param] - val[param + '_pred']) < std_threshold[param], param + '_anomaly_pred'] = 0
-val['anomaly_pred'] = np.where(val[[param + '_anomaly_pred' for param in parametres]].sum(axis = 1) > 0, 1, 0)
-print(pd.crosstab(val['anomaly'], val['anomaly_pred']))
-print(classification_report(val['anomaly'], val['anomaly_pred']))
-print('precision', precision_score(val['anomaly'], val['anomaly_pred'], zero_division=0))
-print('recall', recall_score(val['anomaly'], val['anomaly_pred'], zero_division=0))
-print(f1_score(val['anomaly'], val['anomaly_pred']))
+test['is_test'] = 1
 
-val['is_test'] = 1
-
-val.rename(columns = dict(zip(parametres, [param + '_origine' for param in parametres]))).to_csv(path + '/val_result_' + fichier_suffixe + '_' + str(i) + '.csv', index = False) # sep = ';', retiré pour compatibilité avec meteo_model
+test.rename(columns = dict(zip(parametres, [param + '_origine' for param in parametres]))).to_csv(path + '/test_result_' + fichier_suffixe + '_' + str(i) + '.csv', index = False) # sep = ';', retiré pour compatibilité avec meteo_model
 
 ### l'autoencodeur ne prédit pas les anomalies! Il semble s'appuyer trop sur les données fournies
-FN = val.loc[(val.anomaly == 1) & (val.anomaly_pred == 0), parametres + [param +'_pred' for param in parametres] + [param + '_corrige' for param in parametres]]
+FN = test.loc[(val.anomaly == 1) & (test.anomaly_pred == 0), parametres + [param +'_pred' for param in parametres] + [param + '_corrige' for param in parametres]]
 ## validation totale
-val_loss = tf.keras.losses.mae(y_val, X_val_pred)
-anomalie_val_total = np.where(val_loss > threshold_total, 1, 0)
-val = pd.read_csv(path + '/val_preprocessed_' + fichier_suffixe + '.csv', sep = ';')
-#val['anomaly'] = np.where(val[[param + '_anomaly' for param in parametres]].sum(axis = 1) > 0, 1, 0)
-val['anomaly_pred'] = anomalie_val_total
-print(pd.crosstab(val['anomaly'], val['anomaly_pred']))
-print(classification_report(val['anomaly'], val['anomaly_pred']))
-print('precision', precision_score(val['anomaly'], val['anomaly_pred'], zero_division=0))
-print('recall', recall_score(val['anomaly'], val['anomaly_pred'], zero_division=0))
-print(f1_score(val['anomaly'], val['anomaly_pred']))
+test_loss = tf.keras.losses.mae(y_test, X_test_pred)
+anomalie_test_total = np.where(test_loss > threshold_total, 1, 0)
+test = pd.read_csv(path + '/test_preprocessed_' + fichier_suffixe + '.csv', sep = ';')
+#test['anomaly'] = np.where(test[[param + '_anomaly' for param in parametres]].sum(axis = 1) > 0, 1, 0)
+test['anomaly_pred'] = anomalie_test_total
+print(pd.crosstab(test['anomaly'], test['anomaly_pred']))
+print(classification_report(test['anomaly'], test['anomaly_pred']))
+print('precision', precision_score(test['anomaly'], test['anomaly_pred'], zero_division=0))
+print('recall', recall_score(test['anomaly'], test['anomaly_pred'], zero_division=0))
+print(f1_score(test['anomaly'], test['anomaly_pred']))
 
 
 ##validation_recentes
 #threshold = np.max(train_ae_loss, axis = 0)
-X_val_recentes_pred = model.predict(X_val_recentes)
-val_recentes_ae_loss_param = np.abs(X_val_recentes_pred - y_val_recentes)
+X_test_recentes_pred = model.predict(X_test_recentes)
+test_recentes_ae_loss_param = np.abs(X_test_recentes_pred - y_test_recentes)
 
-anomalie_val_recentes = pd.DataFrame(np.where(val_recentes_ae_loss_param > threshold, 1, 0), columns = [param + '_anomaly_pred' for param in parametres])
-anomalie_val_recentes['anomaly_pred'] = np.where(anomalie_val_recentes[[param + '_anomaly_pred' for param in parametres]].sum(axis = 1) > 0, 1, 0)
-X_val_pred_recentes_unscaled = pd.DataFrame(scaler_param.inverse_transform(X_val_recentes_pred), columns = [param + '_pred' for param in parametres])
-val_recentes = pd.read_csv(path + '/val_recentes_preprocessed_' + fichier_suffixe + '.csv', sep = ';')
-val_recentes = pd.concat([val_recentes, anomalie_val_recentes, X_val_pred_recentes_unscaled], axis = 1)
-#val_recentes['anomaly'] = np.where(val_recentes[[param + 'anomaly' for param in parametres]].sum(axis = 1) > 0, 1, 0)
-print(pd.crosstab(val_recentes['anomaly'], val_recentes['anomaly_pred']))
-print(classification_report(val_recentes['anomaly'], val_recentes['anomaly_pred']))
-
-for param in parametres:
-    print(param)
-    #print(FN[param + '_anomaly_pred'].value_counts(normalize=True))
-    # on retire des anomalies les valeur corrigées trop proches des valeurs d'origine
-    val_recentes.loc[np.abs(val_recentes[param] - val_recentes[param + '_pred']) < std_threshold[param], param + '_anomaly_pred'] = 0
-val_recentes['anomaly_pred'] = np.where(val_recentes[[param + '_anomaly_pred' for param in parametres]].sum(axis = 1) > 0, 1, 0)
-print(pd.crosstab(val_recentes['anomaly'], val_recentes['anomaly_pred']))
-print(classification_report(val_recentes['anomaly'], val_recentes['anomaly_pred']))
-print('precision', precision_score(val_recentes['anomaly'], val_recentes['anomaly_pred'], zero_division=0))
-print('recall', recall_score(val_recentes['anomaly'], val_recentes['anomaly_pred'], zero_division=0))
-print(f1_score(val_recentes['anomaly'], val_recentes['anomaly_pred']))
-
-FN = val_recentes[(val_recentes.anomaly_pred == 0) & (val_recentes.anomaly == 1)]
-pd.DataFrame(FN.codearvalis.unique()).isin(train.codearvalis.unique())
-
-
-## A continuer:
-# - normaliser avec valeurs min max par paramètre absolue: (X_test_pred_param < 0).sum(axis = 0)
-#- confronter à anomalies réelles: définir seuil: rmse, plusieurs fois le threshold 
-# analyser les FP FN: quelle différence entre prédiction et valeur origine, valeur corrigée
-#- ajouter pluie?
-#- regarder années 2023-2024
-#-optimiser: ajouter des dropouts?
-#-modèle pour détecter anomalie à partir de sortie autoencodeur!
-# calculer RMSE
-
-""""sur jeux équilibrés
-
-meta_features = ['Altitude', 'Lambert93x', 'Lambert93y', 'month_sin', 'month_cos', 'jourjulien']
-
-### problème standardisation lambert, altitude, joursjuliens
-parametres_origine = [ param + '_origine' for param in parametres ]
-for i, param in enumerate(parametres):
-    print(param)
-    balanced_df_param = pd.read_csv(path +  '/pd_balanced_df_' + param + '.csv', sep = ';')
-    X_test_pred_param= model.predict(balanced_df_param[parametres_origine + meta_features])
-    test_mae_loss_balanced = np.abs(X_test_pred_param - balanced_df_param[parametres_origine])
-    balanced_df_param[param + '_anomalie_pred'] = np.where( test_mae_loss_balanced[param + '_origine'] > threshold[i], 1, 0)
-    balanced_df_param[param + '_pred'] = X_test_pred_param[:,i]
-    print(pd.crosstab(balanced_df_param[param + '_anomalie_threshold'], balanced_df_param[param + '_anomalie_pred']))
-    print(classification_report(balanced_df_param[param + '_anomalie_threshold'], balanced_df_param[param + '_anomalie_pred']))
-    
-    
-"""
+anomalie_test_recentes = pd.DataFrame(np.where(test_recentes_ae_loss_param > threshold, 1, 0), columns = [param + '_anomaly_pred' for param in parametres])
+anomalie_test_recentes['anomaly_pred'] = np.where(anomalie_test_recentes[[param + '_anomaly_pred' for param in parametres]].sum(axis = 1) > 0, 1, 0)
+X_test_pred_recentes_unscaled = pd.DataFrame(scaler_param.inverse_transform(X_test_recentes_pred), columns = [param + '_pred' for param in parametres])
+test_recentes = pd.read_csv(path + '/test_recentes_preprocessed_' + fichier_suffixe + '.csv', sep = ';')
+test_recentes = pd.concat([test_recentes, anomalie_test_recentes, X_test_pred_recentes_unscaled], axis = 1)
+#test_recentes['anomaly'] = np.where(test_recentes[[param + 'anomaly' for param in parametres]].sum(axis = 1) > 0, 1, 0)
+print(pd.crosstab(test_recentes['anomaly'], test_recentes['anomaly_pred']))
+print(classification_report(test_recentes['anomaly'], test_recentes['anomaly_pred']))
--- src/models/autoencodeur_simple_preprocessing.py
+++ src/models/autoencodeur_simple_preprocessing.py
@@ -15,10 +15,10 @@ import joblib
 # balanced_val_param : jeu d'évaluation de la détection d'anomalie équilibré. Sur données brutest non utilisées pendant l'entrainement
 # val_recentes : jeu d'évaluation non utilisé par train, test et val: pour vérifier le comportement de l'autoencodeur sur des données non vues plus récentes
 
-method  = 'autoencsimple'
+method  = 'autoencoder'
 path    = './data/processed/' + method
-fichier = './data/processed/meteo_pivot_cleaned_2010-2024.csv' # doit contenir datetime sans heure, minute ou seconde
-
+SEUIL_ANOMALIE = 0.1
+fichier = f"data/processed/meteo_pivot_cleaned_2010-2024_{SEUIL_ANOMALIE}.csv" 
 parametres =  ['ETP', 'GLOT', 'TN', 'TX'] # pas RR pour l'instant # ['ETP', 'GLOT', 'TN', 'TX'] # pas RR pour l'instant
 ## On se limite dans un premier temps à 2010-2022: éviter les nouvelles stations météo
 train_years = [a for a in range(2010, 2021)]
@@ -27,13 +27,9 @@ test_years = [a for a in range(2021, 2023)]
 # proportion de station météo complètes pour le test: on garde 10% pour le test
 stations_split_test = 0.1
 
-fichier_suffixe = '-'.join(parametres) + '_' + str(np.min(train_years + test_years)) + '-' + str(np.max(train_years + test_years))
+fichier_suffixe = '-'.join(parametres) + '_' + str(np.min(train_years + test_years)) + '-' + str(np.max(train_years + test_years)) + '-' + str(SEUIL_ANOMALIE)
 
-parametres_feature = ['GLOT', 'TN', 'TX'] # pas RR pour l'instant['ETP', 'GLOT', 'TN', 'TX'] # pas RR pour l'instant
-meta_features = ['Altitude', 'Lambert93x', 'Lambert93y', 'month_sin', 'month_cos']
-for param in parametres_feature:
-    if param not in parametres:
-        meta_features.append(param)
+meta_features = ['Altitude', 'Lambert93x', 'Lambert93y', 'day_sin', 'day_cos']
 
 parametres_origine = [ param + '_origine' for param in parametres ]
 
@@ -44,7 +40,7 @@ meteobydate = pd.read_csv(fichier, sep = ';', parse_dates = ['datemesure'])
 meteobydate['anomaly']  = np.where(meteobydate['anomaly'] > 0, 1, 0)
 # fin de à déplacer en preprocessing
 
-val_recentes = meteobydate[~meteobydate.datemesure.dt.year.isin(train_years + test_years)]
+test_recentes = meteobydate[~meteobydate.datemesure.dt.year.isin(train_years + test_years)]
 meteobydate = meteobydate[meteobydate.datemesure.dt.year.isin(train_years + test_years)]
 
 # séparation données de test
@@ -56,90 +52,62 @@ meteobydate['test'] = meteobydate.codearvalis.isin(stations_rand[:nb_stations_te
 print("Proportions de données de test:")
 print(meteobydate.test.value_counts(normalize = True))
 
-test = meteobydate[meteobydate.test].drop(columns = 'test')
+val = meteobydate[meteobydate.test].drop(columns = 'test')
 # on mélange les données d'entrainement pour éviter d'avoir trop d'homogénéité dans les batchs
 train = meteobydate[~meteobydate.test].sample(frac=1).drop(columns = 'test')
 # pour faciliter les traitements, on renomme les colonnes des données val pour correspondre à celles utilisées pour l'entrainement
 # les colonnes 'param' sont renommées en 'param_corrige
 # les colonnes 'param_origine' sont renommées en 'param'
-val = test
-val = val.rename(columns = dict(zip(parametres, [param + '_corrige' for param in parametres])))
-val = val.rename(columns = dict(zip(parametres_origine, parametres)))
-val_recentes = val_recentes.rename(columns = dict(zip(parametres, [param + '_corrige' for param in parametres])))
-val_recentes = val_recentes.rename(columns = dict(zip(parametres_origine, parametres)))
+test = val
+test = test.rename(columns = dict(zip(parametres, [param + '_corrige' for param in parametres])))
+test = test.rename(columns = dict(zip(parametres_origine, parametres)))
+test_recentes = test_recentes.rename(columns = dict(zip(parametres, [param + '_corrige' for param in parametres])))
+test_recentes = test_recentes.rename(columns = dict(zip(parametres_origine, parametres)))
 
 # on enregistre
 train.to_csv(path + '/train_preprocessed_' + fichier_suffixe + '.csv', sep = ';', index = False)
-test.to_csv(path + '/test_preprocessed_' + fichier_suffixe + '.csv', sep = ';', index = False)
 val.to_csv(path + '/val_preprocessed_' + fichier_suffixe + '.csv', sep = ';', index = False)
-val_recentes.to_csv(path + '/val_recentes_preprocessed_' + fichier_suffixe + '.csv', sep = ';', index = False)
+test.to_csv(path + '/test_preprocessed_' + fichier_suffixe + '.csv', sep = ';', index = False)
+test_recentes.to_csv(path + '/test_recentes_preprocessed_' + fichier_suffixe + '.csv', sep = ';', index = False)
 
 # On normalise avant transformation du jeu de données: c'est plus simple. Cela normalise aussi les variables à prédire!
 scaler_meta = MinMaxScaler()
 train[meta_features] = scaler_meta.fit_transform(train[meta_features])
-test[meta_features] = scaler_meta.transform(test[meta_features])
 val[meta_features] = scaler_meta.transform(val[meta_features])
-val_recentes[meta_features] = scaler_meta.transform(val_recentes[meta_features])
+test[meta_features] = scaler_meta.transform(test[meta_features])
+test_recentes[meta_features] = scaler_meta.transform(test_recentes[meta_features])
 
 scaler_param = MinMaxScaler()
 train[parametres] = scaler_param.fit_transform(train[parametres])
-test[parametres] = scaler_param.transform(test[parametres])
 val[parametres] = scaler_param.transform(val[parametres])
-val_recentes[parametres] = scaler_param.transform(val_recentes[parametres])
+test[parametres] = scaler_param.transform(test[parametres])
+test_recentes[parametres] = scaler_param.transform(test_recentes[parametres])
 
 X_train = train[parametres + meta_features]
-X_test  = test[parametres + meta_features]
-X_val   = val[parametres + meta_features]
-X_val_recentes = val_recentes[parametres + meta_features]
+X_val  = val[parametres + meta_features]
+X_test   = test[parametres + meta_features]
+X_test_recentes = test_recentes[parametres + meta_features]
 
 np.save(path + '/np_xtrain_' + fichier_suffixe + '.npy', X_train)
-np.save(path + '/np_xtest_' + fichier_suffixe + '.npy', X_test)
-np.save(path + '/np_xval_' + fichier_suffixe + '.npy', X_val)
-np.save(path + '/np_xval_recentes_' + fichier_suffixe + '.npy', X_val_recentes)
+np.save(path + '/np_xval_' + fichier_suffixe + '.npy', X_test)
+np.save(path + '/np_xtest_' + fichier_suffixe + '.npy', X_val)
+np.save(path + '/np_xtest_recentes_' + fichier_suffixe + '.npy', X_test_recentes)
 joblib.dump(scaler_param, path + '/joblib_scaler_param_' + fichier_suffixe + '.gz')
 
 
-val = pd.read_csv(path + '/val_preprocessed_' + fichier_suffixe + '.csv', sep = ';')
-val[parametres] = scaler_param.transform(val[parametres])
+test = pd.read_csv(path + '/test_preprocessed_' + fichier_suffixe + '.csv', sep = ';')
+test[parametres] = scaler_param.transform(test[parametres])
 
 # on recherche les valeurs minimales de correction (décile 1 pour retirer les cas extrêmes)
-val[[param + '_corrige' for param in parametres]] = scaler_param.transform(val[[param + '_corrige' for param in parametres]].rename(columns = dict(zip([param + '_corrige' for param in parametres], parametres))))
+test[[param + '_corrige' for param in parametres]] = scaler_param.transform(test[[param + '_corrige' for param in parametres]].rename(columns = dict(zip([param + '_corrige' for param in parametres], parametres))))
 quantiles_corrections = []
 for param in parametres:
-    correction = np.abs(val[param + '_corrige'] - val[param])
+    correction = np.abs(test[param + '_corrige'] - val[param])
     quantiles_corrections.append(correction.loc[correction > 0].quantile([0.1])[0.1])
 print(quantiles_corrections)
 # [0.031959629941127, 0.015307964473969246, 0.007352941176470562, 0.0034602076124566894]
-# sans ETP [0.018672828363058707, 0.007462686567164201, 0.0034602076124566894]
-# le 19/03/2025: [0.03700588730025231, 0.03427925000601729, 0.020220588235294046, 0.01730103806228367]
+# le 25/03/2025: [0.02607232968881415, 0.036175897176691396, 0.022058823529411797, 0.029616724738675937]
 
 ################################
-""" 
-## jeux d'évaluation
-## A terminer
-balanced_df = meteobydate[meteobydate.test == True]
-balanced_df = balanced_df.rename(columns = dict(zip(parametres, [param + '_corrige' for param in parametres])))
-# on doit normaliser les paramètres d'origine, mais les meta_features sont déjà normalisées.
-# on les normalise une seconde fois, sans les utiliser
-balanced_normalized = scaler.transform(balanced_df[parametres_origine + meta_features].rename(columns = dict(zip(parametres_origine, parametres))))
-balanced_df.loc[:,parametres_origine] = balanced_normalized[:,0:len(parametres)]
-for param in parametres:
-    df_param  = balanced_df[balanced_df[param + '_anomalie_threshold'] == 1]
-    nb_anomalies = (df_param[param + '_anomalie_threshold'] == 1).sum()
-    balanced_df_param = pd.concat(
-        [balanced_df[balanced_df[param + '_anomalie_threshold'] == 0].sample(nb_anomalies),
-         df_param
-        ])
-    balanced_df_param.to_csv(path +  '/pd_balanced_df_' + param + '.csv', sep = ';', index = False)
 
-
-# A REVOIR:
-# fichier de validation
-data_valid = meteobydate.loc[meteobydate.test == True]
-data_valid.loc[:,parametres + meta_features] = scaler.inverse_transform(data_valid[parametres + meta_features])
-data_valid[data_valid.test == True].to_csv(path + '/meteobydate_valid.csv', sep = ';', index = False)
-
-X_valid = scaler.transform(data_valid[parametres_origine + meta_features].rename(columns = dict(zip(parametres_origine, parametres))))
-
- """
 
